334373b359212343c218ff56847d5c0261f6beac
Mehdi BEN SMAIL <mehdi.ben_smail@edu.univ-fcomte.fr>
2025-11-30 16:43:04 +0100

    Revert "Animations"
    This reverts commit 9528b0e35f40ded10d7d9199c413e5c15c5d0240.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,11 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter, withHashLocation } from '@angular/router'; // <--- Import withHashLocation
 import { routes } from './app.routes';
-import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes, withHashLocation()), provideAnimations()],
+  providers: [
+    provideRouter(routes, withHashLocation()),
+    provideAnimationsAsync()
+  ]
 };
